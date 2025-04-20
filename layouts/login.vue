89d050d6cec0907f4387b4e920e34cf139b0a663
rodrigodjodi <rodrigo.djodi@gmail.com>
2025-04-20 10:44:01 -0300

    Estrutura de páginas

---
+++ layouts/login.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex h-screen bg-green-300">
+            <div class="hidden lg:block w-1/2 bg-cover bg-center bg-blue-200"
+                >
+
+            </div>
+            <div class="w-full lg:w-1/2 flex items-center justify-center">
+                <slot />
+            </div>
+        </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+// usa o layout login
+definePageMeta({
+  layout: 'login'
+})
+
+</script>
+<template>
+    <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm">
+        <h2 class="text-2xl font-bold mb-6 text-center">Login</h2>
+        <form>
+            <div class="mb-4">
+                <label for="email" class="block text-gray-700">Email</label>
+                <input type="email" id="email" class="border border-gray-300 p-2 w-full rounded" required />
+            </div>
+            <div class="mb-4">
+                <label for="password" class="block text-gray-700">Password</label>
+                <input type="password" id="password" class="border border-gray-300 p-2 w-full rounded" required />
+            </div>
+            <button type="submit" class="bg-blue-500 text-white p-2 rounded w-full">Login</button>
+        </form>
+    </div>
+</template>
