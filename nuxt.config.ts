c61e7c42750e47229dec9fb8f644778de3ea9248
rodrigodjodi <rodrigo.djodi@gmail.com>
2025-04-20 10:43:46 -0300

    instalação nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/icon'],
+  css: ['~/assets/css/main.css'],
   devServer: {
     port: 3327
   }
