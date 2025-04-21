bc96585b4a3766c19134b1b3abf417d76810f1ac
rodrigodjodi <rodrigo.djodi@gmail.com>
2025-04-21 06:32:35 -0300

    Instalação vueFire

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/icon'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/icon',
+    'nuxt-vuefire'
+  ],
   css: ['~/assets/css/main.css'],
   devServer: {
     port: 3327
+  },
+  vuefire: {
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID
+    },
+    auth: {
+      enabled: true
+    },
   }
 })
