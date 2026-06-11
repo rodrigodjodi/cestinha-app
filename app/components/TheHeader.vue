7d7065983d21ffc33dfc3bee7077a3e4f38ad36f
rodrigodjodi <rodrigo.djodi@gmail.com>
2026-06-11 11:20:14 -0300

    user menu

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -1,16 +1,11 @@
 <script setup lang="ts">
-
-import { useFirebaseAuth } from 'vuefire'
-import type { NavigationMenuItem } from '@nuxt/ui'
+import type { NavigationMenuItem, DropdownMenuItem } from '@nuxt/ui'
 const pageTitle = useState('pageTitle', () => 'Carregando...')
-const auth = useFirebaseAuth()
 const user = useCurrentUser()
 const route = useRoute()
 const { sair } = useFirebaseUser()
 
-
-
-const items = computed<NavigationMenuItem[]>(() => [{
+const navItems = computed<NavigationMenuItem[]>(() => [{
   label: 'Painel',
   to: '/painel',
   icon: 'i-lucide-house',
@@ -24,26 +19,35 @@ const items = computed<NavigationMenuItem[]>(() => [{
   label: 'Dias',
   icon: 'i-lucide-calendar',
   to: '/dias',
-    active: route.path.startsWith('/dias')
+  active: route.path.startsWith('/dias')
 }, {
   label: 'Jogos',
   icon: 'i-lucide-dices',
   to: '/jogos',
-    active: route.path.startsWith('/jogos')
+  active: route.path.startsWith('/jogos')
 }, {
   label: 'Estatísticas',
   icon: 'i-lucide-chart-bar',
   to: '/estatisticas',
   active: route.path.startsWith('/estatisticas')
 }])
-
+const userMenuItems:DropdownMenuItem[] = [
+  {
+    label: 'Sair', icon: 'i-lucide-log-out',
+    onSelect: sair
+  }
+]
 </script>
+
 <template>
-    <UHeader title="Cestinha">
-        <UNavigationMenu :items="[items]" />
-        <template #right>
-            <UAvatar v-if="user" :name="user.displayName || user.email" :alt="user.displayName!"
-            @click="sair" />
-        </template>
-    </UHeader>
+  <UHeader title="Cestinha">
+    <UNavigationMenu :items="navItems" />
+    <template #right>
+      <UDropdownMenu :items="userMenuItems">
+        <UButton color="neutral" variant="ghost">
+          <UAvatar v-if="user" :name="user.displayName || user.email" :alt="user.displayName!" />
+        </UButton>
+      </UDropdownMenu>
+    </template>
+  </UHeader>
 </template>
