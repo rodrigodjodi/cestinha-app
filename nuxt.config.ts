dabcee3ed7ebb001820857b3a190c0c2fad98a75
rodrigodjodi <rodrigo.djodi@gmail.com>
2026-06-16 21:42:01 -0300

    variáveis de ambiente

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,12 +13,12 @@ export default defineNuxtConfig({
   },
   vuefire: {
     config: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID
+      apiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID
     },
     auth: {
       enabled: true
@@ -26,9 +26,9 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     firebase: {
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY
+      projectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.NUXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.NUXT_PUBLIC_FIREBASE_PRIVATE_KEY
     }
   },
   imports: {
