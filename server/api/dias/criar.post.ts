32f85ae697effa993d010999403c0a5c8a1097d0
rodrigodjodi <rodrigo.djodi@gmail.com>
2026-06-20 16:32:38 -0300

    feat: incializa times em dia

--- server/api/dias/criar.post.ts
+++ server/api/dias/criar.post.ts
@@ -1,6 +1,13 @@
 import { Timestamp } from "firebase-admin/firestore";
 import { adminAuth, adminDb } from "#server/utils/firebase-admin";
 import { baseDiaSchema } from "@/schemas/dia.schema";
+
+const timesIniciais = {
+  time1: { jogadores: [] },
+  time2: { jogadores: [] },
+  time3: { jogadores: [] }
+};
+
 export default defineEventHandler(async (event) => {
   // 1. header
   const authorization = getHeader(event, "authorization");
@@ -13,27 +20,24 @@ export default defineEventHandler(async (event) => {
   const decodedToken = await adminAuth.verifyIdToken(token);
   // 4. body
   const body = await readBody(event);
-  console.log(body);
   // 5. zod
-  const validacao = baseDiaSchema.safeParse(body);
+  const validacao = baseDiaSchema.safeParse({
+    ...body,
+    times: timesIniciais
+  });
   if (!validacao.success)
     throw createError({
       statusCode: 400,
       statusMessage: 'dados-invalidos',
-      message: "Os dados forncedios não passaram pela validação."
+      message: "Os dados fornecidos não passaram pela validação."
     });
   const payload = validacao.data;
   // 6. gravacao
   const diaRef = adminDb.collection("dias").doc();
-  try{
-    await diaRef.set({
-      ...payload,
-      criadoPor: decodedToken.uid,
-      criadoEm: Timestamp.now()
-    })
-  } catch (error) {
-    throw error
-  } finally {
-    return diaRef.id
-  }
+  await diaRef.set({
+    ...payload,
+    criadoPor: decodedToken.uid,
+    criadoEm: Timestamp.now()
+  });
+  return diaRef.id;
 });
